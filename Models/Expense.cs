cb54a7fd9f5868569e00ab60b9849477ec51e28e
Andreas Olsson <andreasolsson2020@outlook.com>
2025-04-02 08:41:17 +0200

    add expense and income models

---
+++ Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿namespace BudgetTrackerAppBlazorWebassembly.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Expense(string name, decimal amount)
+        {
+            Name = name;
+            Amount = amount;
+            Date = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Models/Income.cs
@@ -0,0 +1,18 @@
+﻿namespace BudgetTrackerAppBlazorWebassembly.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+    
+        public DateTime Date { get; set; }
+
+        public Income(string name, decimal amount)
+        {
+            Name = name;
+            Amount = amount;
+            Date = DateTime.UtcNow;
+        }
+    }
+}
