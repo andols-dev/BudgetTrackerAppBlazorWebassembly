4627878cb15478e4aa96a3d53433a4369209d399
Andreas Olsson <andreasolsson2020@outlook.com>
2025-04-02 09:23:29 +0200

    scope services

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using BudgetTrackerAppBlazorWebassembly;
+using BudgetTrackerAppBlazorWebassembly.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IExpensiveService, ExpensiveService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
